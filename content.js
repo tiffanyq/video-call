b41d8c9c6bb0dfe8647bd122fb08004f37b85e64
Tiffany Quon <tiffany.quon@alumni.ubc.ca>
2024-03-30 13:40:39 -0700

    first commit

---
+++ content.js
@@ -0,0 +1,50 @@
+const opening_dialogue = [
+	"oh it was great! oh wow what did i do again?",
+	"the sun was out which was so nice. it's been so rainy",
+	"yeah no absolutely",
+	"yeah i got to spend time outside enjoying the sunshine",
+	"i'm glad you got to enjoy the weather!",
+	"i'm so excited summer is soon!",
+	"i can't believe Q1 is over!",
+	"omg me too!! i can't believe it!",
+	"yeah me too!",
+	"Same!!",
+	"but at least summer is coming!",
+	"yeah",
+	"yeah!",
+	"yeah! ok, do we have a meeting agenda today?",
+	"let's see, let me pull up the meeting notes doc...",
+	"actually i wanted to talk about our current roadmap",
+	"yes, i would love to understand the big picture!",	
+];
+
+const looping_dialogue = [
+	"let me circle back with you all on that soon",
+	"i think we need to do some more info gathering",
+	"when will the follow-up meeting for this be?",
+	"i will take the action item on that",
+	"can we do a deep dive on this next week instead?",
+	"what are the deliverables for this?",
+	"let's figure out those details offline",
+	"maybe we can sync offline next week",
+	"have we aligned on the success metrics yet?"
+];
+
+const post_embarrassment_dialogue = [
+	"hey lemur i think you're not on mute...",
+	"lemur is everything ok?",
+	"hearing sound feedback, could everyone go on mute?",
+	"hmmm could everyone turn off their video?",
+	"hey lemur i'm going to ping you really quick",
+	"lemur is now still a good time?",
+	"hey actually we have no meeting agenda. let's end early!",
+	"lemur, is your microphone working?",
+	"lemur hey were you hacked?",
+	"lemur i think you've been hacked!!",
+	"lemur i think something is happening with your computer maybe!!",
+	"hey lemur? everything good?",
+	"lemur! i dont think your mic and video buttons are working!",
+	"haha weirdddd this video call is acting so silly today!!",
+	"who enabled 'can't end call'? why does this feature exist?",
+	"i think we can sync offline actually!!"
+];
---
+++ script.js
@@ -0,0 +1,158 @@
+let audioOn = true;
+let videoOn = true;
+let haveBeenEmbarrassed = false;
+let colleagueTiles = [];
+let currentColleague = 0;
+let openingDialogueCompleted = false;
+let openingDialogueLine = 0;
+let postEmbarrassmentDialogueLine = -1;
+let loopingDialogueLine = -1;
+
+window.addEventListener("load", function(event) {
+  const join = document.getElementById("join");
+  const audio = document.getElementById("audio");
+  const video = document.getElementById("video");
+  const endCall = document.getElementById("end-call");
+  const embarrassYourself = document.getElementById("embarrass-yourself");
+  const aprilFoolsTooltip = document.getElementById("dismiss-april-fools");
+  const endCallTooltip = document.getElementById("dismiss-end-call");
+  join.addEventListener('click', joinCall, false);
+  audio.addEventListener('click', toggleAudio, false);
+  video.addEventListener('click', toggleVideo, false);
+  endCall.addEventListener('click', showEndCallTooltip, false);
+  embarrassYourself.addEventListener('click', completeEmbarrassment, false);
+  aprilFoolsTooltip.addEventListener('click', closeAprilFoolsTooltip);
+  endCallTooltip.addEventListener('click', closeEndCallTooltip);
+
+  colleagueTiles = document.querySelectorAll(".speech-bubble");
+  setInterval(advanceMeetingDialogue, 3000);
+  // start with colleague one (index 0)
+  document.getElementById("bubble-1").style.display = "block";
+});
+
+function joinCall() {
+  document.getElementById("screen-1").style.display = "none";
+  document.getElementById("screen-2").style.display = "block";
+}
+
+function toggleAudio() {
+  const audio = document.getElementById("audio");
+  const audioIcon = document.getElementById("audio").querySelector("span");
+  audioOn = !audioOn;
+
+  if (audioOn) {
+    audio.style.backgroundColor = "#ffffff";
+    audio.style.color = "#000000";
+    audioIcon.innerText = "mic";
+  } else {
+    audio.style.backgroundColor = "#fa4d4d";
+    audio.style.color = "#ffffff";
+    audioIcon.innerText = "mic_off";
+  }
+
+  timeToBeEmbarrassedCheck();
+  showAprilFoolsTooltip(); // checks for post-embarrassment in this function
+  closeEndCallTooltip();
+}
+
+function toggleVideo() {
+  const video = document.getElementById("video");
+  const videoIcon = document.getElementById("video").querySelector("span");
+  const selfTileImg = document.getElementById("self-tile-screen-2").querySelector("img");
+
+  videoOn = !videoOn;
+
+  if (videoOn) {
+    video.style.backgroundColor = "#ffffff";
+    video.style.color = "#000000";
+    videoIcon.innerText = "videocam";
+    selfTileImg.src = "img/you.jpg";
+  } else {
+    video.style.backgroundColor = "#fa4d4d";
+    video.style.color = "#ffffff";
+    videoIcon.innerText = "videocam_off";
+    selfTileImg.src = "img/you_camera_off.jpg";
+  }
+
+  timeToBeEmbarrassedCheck();
+  showAprilFoolsTooltip(); // checks for post-embarrassment in this function
+  closeEndCallTooltip();
+}
+
+function timeToBeEmbarrassedCheck() {
+  if (!videoOn && !audioOn && !haveBeenEmbarrassed) setUpForEmbarrassment();
+}
+
+function setUpForEmbarrassment() {
+  document.getElementById("embarrass-yourself").style.display = "block";
+}
+
+function completeEmbarrassment() {
+  document.getElementById("embarrass-yourself").style.display = "none";
+  haveBeenEmbarrassed = true;
+  showAprilFoolsTooltip();
+  document.getElementById("page-title").innerText = ".........";
+  document.body.classList.add("embarrassing-background");
+}
+
+function showAprilFoolsTooltip() {
+  if (haveBeenEmbarrassed) {
+    document.getElementById("april-fools-tooltip").style.display = "block";
+  }
+}
+
+function closeAprilFoolsTooltip() {
+  document.getElementById("april-fools-tooltip").style.display = "none";
+}
+function showEndCallTooltip() {
+  document.getElementById("end-call-tooltip").style.display = "block";
+  closeAprilFoolsTooltip();
+}
+
+function closeEndCallTooltip() {
+  document.getElementById("end-call-tooltip").style.display = "none";
+}
+
+function advanceMeetingDialogue() {
+  let selectedTileIndex = currentColleague;
+  
+  while (selectedTileIndex === currentColleague) {
+    selectedTileIndex = Math.floor(Math.random() * colleagueTiles.length);
+  }
+  currentColleague = selectedTileIndex;
+
+  for (let i = 0; i < colleagueTiles.length; i++) {
+    if (i === selectedTileIndex) {
+      colleagueTiles[i].style.display = "block";
+      // post-embarrassment dialogue
+      if (haveBeenEmbarrassed) {
+        let dialogueLine = postEmbarrassmentDialogueLine;
+        do {
+          dialogueLine = Math.floor(Math.random() * post_embarrassment_dialogue.length);
+        } while (dialogueLine === postEmbarrassmentDialogueLine);
+        postEmbarrassmentDialogueLine = dialogueLine;
+        colleagueTiles[i].innerText = post_embarrassment_dialogue[dialogueLine];
+      }
+      else {
+      // opening dialogue
+      if (!openingDialogueCompleted) {
+        colleagueTiles[i].innerText = opening_dialogue[openingDialogueLine];
+        openingDialogueLine++;
+        if (openingDialogueLine === opening_dialogue.length) {
+          openingDialogueCompleted = true;
+        }
+      } else {
+      // looping dialogue
+        let dialogueLine = loopingDialogueLine;
+        do {
+          dialogueLine = Math.floor(Math.random() * looping_dialogue.length);
+        } while (dialogueLine === loopingDialogueLine);
+        loopingDialogueLine = dialogueLine;
+        colleagueTiles[i].innerText = looping_dialogue[dialogueLine];
+      }
+      }
+    } else {
+      colleagueTiles[i].style.display = "none";
+    }
+  }
+}
